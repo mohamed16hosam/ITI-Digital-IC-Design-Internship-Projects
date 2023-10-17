2a7295e0b4a44a7fb89c56525772665b1f932c18
mohamed16hosam <82730843+mohamed16hosam@users.noreply.github.com>
2023-10-17 17:33:41 +0300

    Delete RISC_V_With_Cache directory

--- RISC_V_With_Cache/PCPlus4.v
+++
@@ -1,8 +0,0 @@
-module PCPlus4 (
-	input wire				[31:0]				PC,
-	output wire			[31:0]				PCPlus4
-);
-
-assign PCPlus4=PC+32'b100 ;
-
-endmodule
--- RISC_V_With_Cache/PCTarget.v
+++
@@ -1,9 +0,0 @@
-module PCTarget (
-	input wire				[31:0]				PC,
-	input wire				[31:0]				ImmExt,
-	output wire			[31:0]				PCTarget
-);
-
-assign PCTarget=PC+ImmExt ;
-
-endmodule
