b068a831eeef4a7a58e4750c5f416619dfb07f30
mohamed16hosam <82730843+mohamed16hosam@users.noreply.github.com>
2023-10-17 17:24:54 +0300

    Delete Sequential_Multiplier_(VHDL)/work directory

--- Sequential_Multiplier_(VHDL)/work/seq_multiplier_tb/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity seq_multiplier_tb is
-    generic(
-        N               : integer := 4
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of N : constant is 1;
-end seq_multiplier_tb;
